1e1edd3f91a23ff21788b632102c8eddde70154d
Eugene G <e.gapanovich@invento.by>
2023-09-25 22:00:30 +0300

    added responsive styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Gui from '../Gui';
 import Scene from '../Scene';
 
+import style from './style.module.scss';
+
 const App = () => {
   return (
-    <>
+    <div className={style.wrapper}>
       <Gui />
       <Scene />
-    </>
+    </div>
   );
 };
 
